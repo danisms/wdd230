4bffbf9e6a249752ee557663f683a39658bc0e3d
Daniel C. Opute <danielcopute@gmail.com>
2024-04-27 20:49:45 +0100

    Completed text-pre-web-knowledge.

---
+++ scripts/getDates.js
@@ -0,0 +1,13 @@
+// get elements
+const currentYearElement = document.querySelector('#footerCurrentYear');
+const lastModifiedElement = document.querySelector('#lastModified');
+
+// get and inset values into the elements
+let currentDateAndTime = new Date()
+let currentYear = currentDateAndTime.getFullYear();
+let lastModified = document.lastModified;
+
+currentYearElement.innerHTML = currentYear;
+lastModifiedElement.innerHTML = `Last Modification: ${lastModified}`;
+
+
