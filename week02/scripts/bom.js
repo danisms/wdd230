949604081698495fbf0ae7586694d48b832aea1d
Daniel C. Opute <danielcopute@gmail.com>
2024-05-02 18:46:31 +0100

    comleted js DOM manipulation activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,31 @@
+// Get all html elements needed
+const userInput = document.querySelector('#favchap');
+const addBtn = document.querySelector('button');
+const scriptureList = document.querySelector('#list');
+
+addBtn.addEventListener('click', () => {
+    if (userInput.value != "") {
+        // create a list element
+        const newList = document.createElement('li');
+        // create a button element and set it properties
+        const delBtn = document.createElement('button')
+        delBtn.setAttribute('class', 'del-btn');
+        delBtn.textContent = "❌";
+        // populate the li elements with the input value
+        newList.innerHTML = userInput.value;
+        newList.append(delBtn);
+        scriptureList.appendChild(newList);
+
+        // add an event listener to the delete button
+        // this will remove the child element on click
+        delBtn.addEventListener('click', () => {
+            scriptureList.removeChild(newList);
+            userInput.focus();
+            userInput.value = "";
+        })
+
+    }else {
+        alert('Please, Enter a chapter.')
+        userInput.focus();
+    }
+})
