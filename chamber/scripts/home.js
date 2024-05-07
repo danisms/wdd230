81ae5d64eb1b5d3046b554ad880a50184b4b3d8c
Daniel C. Opute <danielcopute@gmail.com>
2024-05-07 14:25:13 +0100

    completed-homepage

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -1,7 +1,7 @@
 // get nav element and style it
 const navElement = document.querySelector('nav');
 const navElementInitialColor = navElement.style.getPropertyValue('background-color');
-console.log(navElementInitialColor);  // for testing purpose
+// console.log(navElementInitialColor);  // for testing purpose
 
 // display menu with menu button on small screens
 const menuBtn = document.querySelector('#menu');
@@ -17,6 +17,7 @@ menuBtn.addEventListener('click', ()=> {
 // const h2HeadersBgColor = h2HeadersElement.style.getPropertyValue('background-color');
 // console.log(h2HeadersBgColor);  // for testing purpose
 const h2HeadersBgColor = '#094074';
+const navBgColor = 'rgba(0, 0, 0, 0.9)';
 
 let scrolled = 0;
 window.addEventListener("scroll", () => {
@@ -35,7 +36,11 @@ window.addEventListener("scroll", () => {
         
         if (current_y_scroll_pos > 600) {
             // set h2Headers Background color for nav bg-color
-            navElement.style.backgroundColor = h2HeadersBgColor;
+            if (window.innerWidth >= 890) {
+                navElement.style.backgroundColor = h2HeadersBgColor;
+            }else {
+                navElement.style.backgroundColor = navBgColor;
+            }
         }else {
             navElement.style.backgroundColor = navElementInitialColor;
         }
@@ -92,7 +97,7 @@ function scroll(scrollBox, scrollElements, visibleElements, scrollMax, initialSc
 function setNumOfVisibleElements(classElement) {
     let NumOfVisibleElements = 0;
     let allElements = document.querySelectorAll(classElement);
-    console.log(parseInt(window.innerWidth));
+    // console.log(parseInt(window.innerWidth));  // for testing purpose
     if (parseInt(window.offsetWidth) >= 890) {
         allElements.forEach((element)=> {
             element.style.width = '30.5%;';
@@ -204,7 +209,7 @@ eventRightBtn.addEventListener('click', () => {
     let scrollList = scroll(eventScrollBox, eventScrollElements, eventNumOfVisibleElements, eventScrollMax, eventInitialScrollElements, eventScrollLayer, eventScrollWidth, eventScrollCount, true);
     eventScrollElements = scrollList[0];
     eventScrollWidth = scrollList[1];
-    // console.log(weDoScrollWidth);  // for testing purpose
+    // console.log(eventScrollWidth);  // for testing purpose
 });
 
 // set left button click
@@ -213,37 +218,64 @@ eventLeftBtn.addEventListener('click', () => {
     let scrollList = scroll(eventScrollBox, eventScrollElements, eventNumOfVisibleElements, eventScrollMax, eventInitialScrollElements, eventScrollLayer, eventScrollWidth, eventScrollCount, false);
     eventScrollElements = scrollList[0];
     eventScrollWidth = scrollList[1];
-    // console.log(weDoScrollWidth);  // for testing purpose
+    // console.log(eventScrollWidth);  // for testing purpose
 });
 
 // Event Description Display Handler
 let randomNum = 0;
+let writing = '';  // global variable to set writing interval.
+let delayFunctionCall = 10000;
 
 // Display event description
 function displayDescription(elementId) {
-    const ele = document.querySelector(`#${elementId}`);
-    ele.style.top = 0;
-    ele.style.left = 0;
+    // delay function for home title intro text writing display to complete
+    setTimeout(() => {
+        const ele = document.querySelector(`#${elementId}`);
+        const paragraph = ele.children[1];
+        const text = ele.children[2];
+        ele.style.top = 0;
+        ele.style.left = 0;
+
+        // display righting
+        // console.log(ele.children);  // for testing purpose
+        // console.log(paragraph);  // for testing purpose
+        const textValue = text.textContent;
+        // console.log(textValue);  // for texting purpose;
+        // call function to display paragraph by letters;
+        textByLetter(textValue, paragraph, 80);
+    }, delayFunctionCall);  // this is set to zero once title intro is complete; check downward for the call.
 }
+
 // Remove event description
 function removeDescription(elementId, direction) {
-    randomNum = Math.random() * 4
-    randomNum = Math.ceil(randomNum)
-    // console.log(randomNum);  // for testing purpose
-    const ele = document.querySelector(`#${elementId}`);
-    if (randomNum == 1) {
-        ele.style.top = direction;
-        // console.log('remove top');  // for testing purpose
-    }else if (randomNum == 2) {
-        ele.style.left = `-${direction}`;
-        // console.log('remove right');
-    }else if (randomNum == 3) {
-        ele.style.top = `-${direction}`;;
-        // console.log('remove bottom');
-    }else {
-        ele.style.left = direction;
-        // console.log('remove left');
-    }
+        // delay function for intro text writing display to complete
+        setTimeout(() => {
+            randomNum = Math.random() * 4
+        randomNum = Math.ceil(randomNum)
+        // console.log(randomNum);  // for testing purpose
+        const ele = document.querySelector(`#${elementId}`);
+        const paragraph = ele.children[1];
+        if (randomNum == 1) {
+            ele.style.top = direction;
+            // console.log('remove top');  // for testing purpose
+        }else if (randomNum == 2) {
+            ele.style.left = `-${direction}`;
+            // console.log('remove right');
+        }else if (randomNum == 3) {
+            ele.style.top = `-${direction}`;;
+            // console.log('remove bottom');
+        }else {
+            ele.style.left = direction;
+            // console.log('remove left');
+        }
+
+        clearInterval(writing);  // for clearing writing interval set by textByLetter function.
+        // enable all buttons disabled by textByLetter function.
+        const allBtn = document.querySelectorAll('button')
+        allBtn.forEach((button)=>{
+            button.disabled = false;
+        });
+    }, delayFunctionCall);
 }
 
 // Spotlight section
@@ -251,3 +283,52 @@ function changeVisibility(elementId, opacity) {
     const advertTextDesc = document.querySelector(`#${elementId}`);
     advertTextDesc.style.opacity = opacity;
 }
+
+// write out text by letter
+function textByLetter(text, element, speed) {
+    let ele = element
+    if (element[0] == '#' || element[0] == '.') {
+        ele = document.querySelector(element);
+    }else {
+        // ele remains element
+    }
+    let textList = text.split('');
+    let indexNum = 0;
+    let displayText = '';
+    // Disable all buttons
+    const allBtn = document.querySelectorAll('button')
+    allBtn.forEach((button)=>{
+        button.disabled = true;
+    });
+    // console.log(textList)
+    // console.log(textList.length)
+    function writeText() {
+        let textValue = textList[indexNum];
+        displayText += textValue;
+        ele.innerHTML = displayText;
+        indexNum++;
+        if (indexNum >= textList.length) {
+            clearInterval(writing)
+            allBtn.forEach((button)=>{
+                button.disabled = false;
+            });
+        };
+    };
+    writing = setInterval(() => {
+        writeText();
+    }, speed);
+};
+
+// display page title
+const homeTitle = document.getElementById('head-title');
+homeTitle.style.opacity = '1';
+textByLetter('Benin Hotel Chamber of Commerce', '#head-title', 75);
+setTimeout(() => {
+    const headMotto = document.querySelector('#head-motto')
+    // headMotto.style.display = 'block';
+    headMotto.style.opacity = '1';
+    textByLetter('Connecting Hospitality, Driving Growth', '#head-motto', 75);
+    setTimeout(() => {
+        delayFunctionCall = 0;
+    }, 3500);
+}, 3700);
